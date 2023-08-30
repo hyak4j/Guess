0b455cb1407d8cf1ff103b11bcbbe91e3a30f553
hyak4j <65548056+hyak4j@users.noreply.github.com>
2023-08-30 23:02:22 +0800

    initial commit

---
+++ app/src/main/java/com/hyak4j/guess/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hyak4j.guess
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        R.drawable.icon_penguin
+    }
+}
