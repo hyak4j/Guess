7fd520f8c927aea57810c1bd32ad1a9169829385
hyak4j <65548056+hyak4j@users.noreply.github.com>
2023-09-03 22:39:48 +0800

    commit mail box

--- app/src/main/java/com/hyak4j/guess/MainActivity.kt
+++ app/src/main/java/com/hyak4j/guess/MainActivity.kt
@@ -4,9 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    val secretNumber = SecretNumber()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        R.drawable.icon_penguin
+        setContentView(R.layout.activity_box)
+
     }
 }
