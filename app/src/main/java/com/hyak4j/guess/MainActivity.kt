0b4628b612afbf176d91734148a3f20ae9830dcd
hyak4j <65548056+hyak4j@users.noreply.github.com>
2023-09-19 22:22:41 +0800

    [新增] MainActivity 列表；RecyclerView內部類別

--- app/src/main/java/com/hyak4j/guess/MainActivity.kt
+++ app/src/main/java/com/hyak4j/guess/MainActivity.kt
@@ -1,43 +1,73 @@
 package com.hyak4j.guess
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.hyak4j.guess.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    val secretNumber = SecretNumber()
     val TAG = MainActivity::class.java.simpleName
     private lateinit var binding: ActivityMainBinding
+    val functions = listOf<String>(
+        "Camera",
+        "Guess Game",
+        "Record List",
+        "Invite friend",
+        "Parking",
+        "Download coupons",
+        "News",
+        "Maps"
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        Log.d(TAG, "secret number: ${secretNumber.secret}")
+
+        //RecyclerView
+        var recycler = binding.recycler;
+        recycler.layoutManager = LinearLayoutManager(this)
+        // 固定大小
+        recycler.setHasFixedSize(true)
+        recycler.adapter = FunctionAdapter()
+    }
+
+    inner class FunctionAdapter() : RecyclerView.Adapter<FunctionHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FunctionHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.row_function, parent, false)
+            val holder = FunctionHolder(view)
+            return holder
+        }
+
+        override fun getItemCount(): Int {
+            return functions.size
+        }
+
+        // 有資料時會呼叫
+        override fun onBindViewHolder(holder: FunctionHolder, position: Int) {
+            holder.nameText.text = functions.get(position)
+            holder.itemView.setOnClickListener {
+                functionClicked(position)
+            }
+        }
 
     }
 
-    fun check(view : View){
-        // btn_ok
-        val num = binding.number.text.toString().toInt()
-        println("number: $num")
-        Log.d(TAG, "Input number: $num")
-        val different = secretNumber.validate(num)
-        var message = getString(R.string.you_got_it)
-        if (different < 0){
-            message = getString(R.string.bigger)
-        }else if (different > 0){
-            message = getString(R.string.smaller)
+    private fun functionClicked(position: Int) {
+        when(position){
+            1 -> startActivity(Intent(this, MaterialActivity::class.java))
+            else -> return
         }
-        AlertDialog.Builder(this)
-            .setTitle(getString(R.string.message))
-            .setMessage(message)
-            .setPositiveButton(getString(R.string.ok), null)
-            .show()
-        // Toast
-//        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+    class FunctionHolder(view: View) : RecyclerView.ViewHolder(view){
+        // 暫存
+        var nameText: TextView = view.findViewById(R.id.name)
     }
 }
